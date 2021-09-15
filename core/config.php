e4189185d105af487b51a4cb719b3f83d282068c
ronaldidro <81252009+ronaldidro@users.noreply.github.com>
2021-09-14 19:26:48 -0500

    Update Config

--- core/config.php
+++ core/config.php
@@ -1,8 +1,8 @@
 <?php
   define('URL', 'http://localhost/foody/');
 
-  define('HOST', 'localhost');
-  define('DB', 'foody-bd');
-  define('USER', 'root');
-  define('PASSWORD', "");
+  define('HOST', 'j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+  define('DB', 'x5xq3p9ae5oon5bu');
+  define('USER', 'ttklp0ask1q4livs');
+  define('PASSWORD', 'e8v444q7vi4haxaw');
   define('CHARSET', 'utf8mb4');
